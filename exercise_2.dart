4703bcd11439a50519e5fbf8a8692c1591520993
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2022-08-20 01:35:53 +0530

    chore: commit save point

--- exercise_2.dart
+++ exercise_2.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  stdout.write("Hi, please choose a number:");
-  final number = int.parse(stdin.readLineSync()!);
+  stdout.write("Hi, please choose a number: ");
+  final input = stdin.readLineSync();
 
-  if (number.isEven) {
-    print("Chosen number is even");
-  } else {
-    print("Chosen number is odd");
+  if (input is String) {
+    final number = int.parse(input);
+    if (number.isEven) {
+      print("Chosen number is even");
+    } else {
+      print("Chosen number is odd");
+    }
   }
 }
